2475831d8265285c65643b99ba9dbc57e22293a0
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-04-05 22:31:46 -0300

    chore: finished application template

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import incomeImage from '../../assets/income.svg';
+import outcomeImage from '../../assets/outcome.svg';
+import totalImage from '../../assets/total.svg';
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImage} alt="Icone de Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImage} alt="Icone de Entradas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+      <div className='highlight-background'>
+        <header>
+          <p>Total</p>
+          <img src={totalImage} alt="Icone de Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,11 +5,12 @@ const GlobalStyle = createGlobalStyle`
     --background: #f0f2f5;
     --red: #e52e4d;
     --blue: #5429cc;
+    --green: #33cc95;
 
     --blue-light: #6933ff;
 
-    --text-title: #e52e4d;
-    --text-body: #e52e4d;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
 
     --background: #f0f2f5;
     --shape: #ffffff;
