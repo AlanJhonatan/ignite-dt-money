5b6c9af82b4d3d235bedfd34bf73025f4ba9f24e
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-04-14 15:41:01 -0300

    refactor: using Transaction model in miraje to create/read

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,35 @@ createServer({
   models: {
     transaction: Model
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Desenvolvimento de sites',
+          amount: 6000,
+          createdAt: new Date('2021-03-14 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Desenvolvimento de sites',
+          amount: 4000,
+          createdAt: new Date('2021-03-15 09:00:00')
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return this.schema.all('transactions');
+      return this.schema.all('transaction');
     });
 
     this.post('/transactions', (schema, request) => {
